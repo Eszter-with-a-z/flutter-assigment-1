bcde661a302fee29b4924295234acc5118ef997a
Eszter-with-a-z <85640262+Eszter-with-a-z@users.noreply.github.com>
2025-06-30 21:57:18 +0200

    Add image and info with icons

--- flutter_assignment_1/lib/main.dart
+++ flutter_assignment_1/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-
+final Uri _url = Uri.parse('https://www.linkedin.com/in/eszter-kov%C3%A1cs-a40826213/');
 void main() {
     runApp(MyApp());
 }
@@ -37,6 +38,7 @@ class LayoutPage extends StatelessWidget{
                 child:Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children:[
+                        Image.asset('images/profile.jpg'),
                         Text('Kovács Eszter',
                             style: GoogleFonts.windSong(
                                 fontSize: 32,
@@ -45,11 +47,47 @@ class LayoutPage extends StatelessWidget{
                         Text('user-centered design specialst',
                             style: TextStyle(
                                 fontWeight: FontWeight.bold
-                            ),
+                            )                     
+                        ),
+                        Column(
+                            
+                            children:[ 
+                            Row(   
+                                mainAxisAlignment: MainAxisAlignment.center, 
+                                children: [
+                                    Icon(Icons.email_rounded),
+                                    Text('eszter.with.a.z@gmail.com')
+                                ]
+                        ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                                Icon(Icons.phone_android_rounded),
+                                Text('+46 79 355 2542')
+                            ]
+                        ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                                Icon(Icons.web_rounded),
+                                ElevatedButton(
+                                    onPressed: _launchUrl,
+                                    child: Text('LinkedIn'),
+                                    )
+            
+                            ]
                         )
+                    ])
+                        
                     ]
                 ),
             )
         );
     }
+}
+ // Tutorial: https://pub.dev/packages/url_launcher
+Future<void> _launchUrl() async {
+  if (!await launchUrl(_url)) {
+    throw Exception('Could not launch $_url');
+  }
 }
